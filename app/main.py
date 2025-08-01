ddede63341e4bd82f951bfc0a78d1ac5883ef81b
Ankit Kandal <your-email@example.com>
2025-08-01 15:06:43 +0530

    this is to change the hardcoded local host of my computer because this caused the site to run only when the server was running on my system

--- app/main.py
+++ app/main.py
@@ -90,10 +90,12 @@ async def api_info():
 
 # For local dev
 if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True,
         log_level="info"
     )
